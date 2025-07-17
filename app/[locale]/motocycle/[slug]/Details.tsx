b73b08a1d89ae9bcdb30b25a8d271c91bd6defdc
Ahmed Waleed <awaled@devsolutions.team>
2025-07-17 17:56:50 +0300

    single motocycle fix

--- app/[locale]/motocycle/[slug]/Details.tsx
+++ app/[locale]/motocycle/[slug]/Details.tsx
@@ -86,11 +86,11 @@ const Details = ({ params }: { params: { slug: string } }) => {
     setFields(initialFields(t));
   }, [t]);
 
-  const galleryImages =
-    motorcycle?.gallery?.map((img: any) => ({
-      url: img.url,
-      alt: img.alt || img.title,
-    })) || [];
+  // const galleryImages = motorcycle?.gallery ?
+  //   motorcycle?.gallery?.map((img: any) => ({
+  //     url: img.url,
+  //     alt: img.alt || img.title,
+  //   })) : [];
 
   const deliverableImages = motorcycle?.deliverables
     ? motorcycle?.deliverables.map((img: any) => ({
